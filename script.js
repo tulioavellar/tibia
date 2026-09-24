58b737d97415732004d3339c530eaf0e4de36119
Tulio Avellar <105487326+tulioavellar@users.noreply.github.com>
2026-09-24 11:06:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,39 @@ const ctx = cnv.getContext("2d");
 cnv.width = innerWidth;
 cnv.height = innerHeight;
 
+//Definir a Sprite usada
+const spriteMapa = new Image();
+spriteMapa.src = "src/sprites/mapa.png";
+
 //Desenhar na tela
-ctx.fillRect(0,0,40,40);
+
+class Sqm {
+  constructor(contexto, x, y, w, h, sprite, sx, sy, sw, sh, px, py, pw, ph) {
+    this.sprite = {
+      img: sprite,
+      x: sx,
+      y: sy,
+      w: sw,
+      h: sh,
+      px: px,
+      py: py,
+      pw: pw,
+      ph: ph
+    };
+    
+    this.position = {
+      x: 0,
+      y: 0
+    };
+    
+    this.size = {
+      w: w,
+      h: h
+    };
+  }
+}
+
+//Mostrar o SQM
+let meuSqm1 = new Sqm(ctx, 0, 0, 40, 40, spriteMapa, 0, 0, 40, 40, 0, 0, 40, 40);
+
+console.log(meuSqm1);
