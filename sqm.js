4be188952459bb2655d1f4ef66db152798710da7
Tulio Avellar <105487326+tulioavellar@users.noreply.github.com>
2026-09-24 14:33:45 -0300

    Create sqm.js

---
+++ sqm.js
@@ -0,0 +1,30 @@
+export default class Sqm {
+  constructor(contexto, x, y, w, h, sprite, sx, sy, sw, sh, px, py, pw, ph) {
+    this.contexto = contexto;
+    this.sprite = {
+      img: sprite,
+      x: sx,
+      y: sy,
+      w: sw,
+      h: sh,
+      px: px,
+      py: py,
+      pw: pw,
+      ph: ph
+    };
+    
+    this.position = {
+      x: 0,
+      y: 0
+    };
+    
+    this.size = {
+      w: w,
+      h: h
+    };
+  }
+  //Renderizar o SQM
+  renderiza() {
+    this.contexto.drawImage(this.sprite.img, this.sprite.x, this.sprite.y, this.sprite.w, this.sprite.h, this.sprite.px, this.sprite.py, this.sprite.pw, this.sprite.ph);
+  }
+}
