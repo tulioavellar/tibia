f004286dc6a490a995e57bb61614a39467c21bbe
Tulio Avellar <105487326+tulioavellar@users.noreply.github.com>
2026-09-24 09:24:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const cnv = document.querySelector("#mapa");
 const ctx = cnv.getContext("2d");
 
+//Definir tamanho do Canvas
+cnv.width = innerWidth;
+cnv.height = innerHeight;
+
+//Desenhar na tela
 ctx.fillRect(0,0,40,40);
