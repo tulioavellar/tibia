914fee1d525476543222e89d516d85825c671076
Tulio Avellar <105487326+tulioavellar@users.noreply.github.com>
2026-09-24 14:30:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -47,11 +47,13 @@ function startGame() {
 }
 
 //Mostrar o SQM
-let meuSqm1 = new Sqm(ctx, 0, 0, 32, 32, spriteMapa, 0, 0, 32, 32, 0, 0, 32, 32);
+let meuSqm1 = new Sqm(ctx, 0, 0, 32, 32, spriteMapa, 0, 0, 32, 32, 64, 64, 32, 32);
+let meuSqm2 = new Sqm(ctx, 0, 0, 32, 32, spriteMapa, 0, 0, 32, 32, 0, 0, 32, 32);
 
 console.log(meuSqm1);
 
 meuSqm1.renderiza();
+meuSqm2.renderiza();
 
 //Carregar o jogo
 spriteMapa.onload = function () {
